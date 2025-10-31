96fb5eb69235403a18282c5d7939e665210491c4
Mohammad Ali <mscsapan@gmail.com>
2025-10-31 22:22:05 +0600

    📝 Basic variable and condition syntex added in templates file

--- basicapp/views.py
+++ basicapp/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('Hello Django')
+    return render(request,'home/home.html',{'name':'Basic App'})
 
 def dashboard(request):
     return render(request,'index.html')

c38d3561796402e21274aa07320b7fee82f49d07
Mohammad Ali <mscsapan@gmail.com>
2025-11-02 22:20:33 +0600

    ✅ Wrap try-except block for unwanted exception

--- basicapp/views.py
+++ basicapp/views.py
@@ -10,35 +10,38 @@ def dashboard(request):
     return render(request,'index.html')
 
 def request_with_param(request,id):
-    # print(f'requested-id {type(id)}')
+    try:
+        # print(f'requested-id {type(id)}')
     
-    converted_id = int(id)
-    
-    filter_data = {}
-    
-    my_data = [
-        {'id': 1,'name':'Mohammad','email':'mohammad@gmail.com'},
-        {'id': 2,'name':'Ali','email':'ali@gmail.com'},
-        {'id': 3,'name':'Khan','email':'khan@gmail.com'},
-        {'id': 4,'name':'Anika','email':'anika@gmail.com'},
-        {'id': 5,'name':'Taha','email':'taha@gmail.com'},
-        {'id': 6,'name':'Taskeen','email':'taha.taaskeen@gmail.com'},
-        {'id': 7,'name':'Ibrahim','email':'ibrahim@gmail.com'},
-    ]
-    
-    for i in my_data:
-        print(f'i is {i}')
-        print(f'loop is type {type(i['id'])} - {converted_id} - {type(converted_id)}')
+        converted_id = int(id)
         
-        if i['id'] == converted_id:
-            filter_data = i
-            break
-    
-    print(f'filter-data {filter_data}')
-    
-    # user_id = ''
-    # if isinstance(id,str):
-    #     user_id = id
-    # elif isinstance(id,int):
-    #     user_id = str(id)
-    return render(request,'params.html',{'id':id,'data':filter_data})
+        filter_data = {}
+        
+        my_data = [
+            {'id': 1,'name':'Mohammad','email':'mohammad@gmail.com'},
+            {'id': 2,'name':'Ali','email':'ali@gmail.com'},
+            {'id': 3,'name':'Khan','email':'khan@gmail.com'},
+            {'id': 4,'name':'Anika','email':'anika@gmail.com'},
+            {'id': 5,'name':'Taha','email':'taha@gmail.com'},
+            {'id': 6,'name':'Taskeen','email':'taha.taaskeen@gmail.com'},
+            {'id': 7,'name':'Ibrahim','email':'ibrahim@gmail.com'},
+        ]
+        
+        for i in my_data:
+            print(f'i is {i}')
+            print(f'loop is type {type(i['id'])} - {converted_id} - {type(converted_id)}')
+            
+            if i['id'] == converted_id:
+                filter_data = i
+                break
+        
+        print(f'filter-data {filter_data}')
+        
+        # user_id = ''
+        # if isinstance(id,str):
+        #     user_id = id
+        # elif isinstance(id,int):
+        #     user_id = str(id)
+        return render(request,'params.html',{'id':id,'data':filter_data})
+    except Exception as e:
+        return HttpResponse(f'Invalid arguments {e}')

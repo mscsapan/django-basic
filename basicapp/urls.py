e5a4dcff9d3043b8c2cc89213cad1e9e5ac3050c
Mohammad Ali <58435612+mscsapan@users.noreply.github.com>
2025-11-21 22:12:20 +0600

    Add profile path to urlpatterns

--- basicapp/urls.py
+++ basicapp/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('',views.home),
     path('dashboard/',views.dashboard,name='dashboard'),
+    path('profile/',views.dashboard,name='profile'),
     path('id/<str:id>',views.request_with_param,name='param-req'),
 ]
